4c813e9164c25d2d996171d294366704709faf55
Kaitou345 <68972628+Kaitou345@users.noreply.github.com>
2023-09-23 13:29:26 +0600

    Update button.ino

--- src/button.ino
+++ src/button.ino
@@ -1,15 +1,15 @@
 int button_read() {
   int cl = 0;
 p:  int t = 0;
-  if (digitalRead(SW) == 0) {
+  if (digitalRead(SWITCH) == 0) {
     digitalWrite(LED, HIGH);
-    while (digitalRead(SW) == 0) {
+    while (digitalRead(SWITCH) == 0) {
       delay(1); t++;
     }
     digitalWrite(LED, LOW);
     if (t > 15) {
       t = 0; cl++;
-      while (digitalRead(SW) == HIGH) {
+      while (digitalRead(SWITCH) == HIGH) {
         delay(1); t++;
         if (t > 1000) return cl;
       }
