ab07b8ed05e7e84b6aa803c28ae537f8a7fcac04
Kaitou345 <68972628+Kaitou345@users.noreply.github.com>
2024-04-25 16:02:21 +0600

    Update first_round.ino

--- src/first_round.ino
+++ src/first_round.ino
@@ -1,34 +1,3 @@
 void first_round(bool isRight) {
   
-  
-  while (true) {
-    turn_check();
-    Serial.println(turn_counter);
-    lens.MainLoop();
-
-    if(should_stop && millis() - stop_timer > 950) {
-      motor.SetSpeed(0);
-      steering.SetRotation(0);
-      break;
-    }
-
-    if(turn_counter >=12 && !should_stop) {
-      should_stop = true;
-      stop_timer = millis();
-
-    }
-    pid.SetPoint(0);
-    pid.SetLimit(SERVO_CLAMP);
-    int output;
-    if (isRight) {
-      output = pid.GetOutputR(COMFORTABLE_DISTANCE);
-    } else {
-      output = pid.GetOutputL(COMFORTABLE_DISTANCE);
-    }
-    motor.SetSpeed(100 - (abs(output) * 1));
-    if (!isRight) {
-      output *= RIGHT;
-    }
-    steering.SetRotation(output);
-  }
 }
